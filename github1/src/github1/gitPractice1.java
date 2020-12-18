be2c6fe35db33e1cdf98e0a658704a152fc6c5da
Vallari Chitnis <chitnisvallari@gmail.com>
2020-12-19 00:19:32 +0530

    committing clonned files!

--- github1/src/github1/gitPractice1.java
+++ github1/src/github1/gitPractice1.java
@@ -5,6 +5,7 @@ public class gitPractice1 {
 	public static void main(String[] args) {
 		
 		System.out.println("i will practice on git using you!");
+		System.out.println("yes done right!!");
 		
 
 	}
